6e877e74ca4308e30f0dbd839e10f6052ee9c397
Leo <leo.reading@gmail.com>
2016-05-28 11:50:38 -0400

    Added support for images in rants, including a lightbox plugin.

--- app/scripts/controllers/rants-controller.js
+++ app/scripts/controllers/rants-controller.js
@@ -3,10 +3,16 @@
 angular.module('devrantAnalyzerApp')
   .controller('RantsController', function ($scope, DevRantService, toastr) {
     $scope.loading = true;
+    $scope.images = [];
     DevRantService.getRants().then(function(result){
       if(result.status === 200){
         $scope.loading = false;
         $scope.rants = result.data.rants;
+        $scope.images = $scope.rants.filter(function (x) {
+            return x.attached_image !== '';
+        }).map(function (x) {
+            return x.attached_image.url;
+        });
       }else{
         toastr.error('Some error in RantsController');
       }
